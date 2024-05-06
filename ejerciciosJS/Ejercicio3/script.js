759cd6dfd43c276eef059af2a3d86d90995d016e
Nicolas Martinez <nicolasmartinezalfonso@gmail.com>
2024-05-06 16:47:59 -0300

    cree una pagina de to-do list

---
+++ ejerciciosJS/Ejercicio3/script.js
@@ -0,0 +1,17 @@
+function agregarElemento() {
+  let lista = document.getElementById("lista");
+  let Inp = document.getElementById("textarea");
+  const button = document.createElement("button");
+
+  const elementoLista = document.createElement("li");
+  elementoLista.textContent = Inp.value;
+  button.textContent = "Delete";
+
+  lista.appendChild(elementoLista);
+  elementoLista.append(button);
+
+  elementoLista.onclick = function () {
+    lista.removeChild(elementoLista);
+  };
+  //
+}
